d00fda41bbbbb65178fc418e865dfa9b3b6b37bc
Sabbir Ahmed <Sabbir Ahmed>
2017-02-14 22:35:03 +0600

    comments added

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,9 @@
 //Software Engineer, Selise rockin' software
 //Git profile: https://github.com/prince3339
 
-(function(window){
+//safe code mode
+// here ; is used to make sure that other codes loaded from different library/framework/file finished
+;(function(window){
  'use strict';
   var vm = window;
 
